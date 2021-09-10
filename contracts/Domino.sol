a2a2449cdcc4ea0c762a6ff0841017f19ca6655a
ycl <yauquenlay@gmail.com>
2021-09-10 16:50:34 +0800

    dom nft

--- contracts/Domino.sol
+++ contracts/Domino.sol
@@ -6,24 +6,36 @@ import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
 import "@openzeppelin/contracts/utils/Counters.sol";
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/utils/math/SafeMath.sol";
 
-contract Domino is ERC721URIStorage,ERC721Enumerable {
+contract Domino is ERC721URIStorage,ERC721Enumerable,Ownable {
 
     using Counters for Counters.Counter;
     using SafeERC20 for IERC20;
+    using SafeMath for uint256;
 
 
     Counters.Counter private _tokenIdTracker;
 
+    uint public constant MAX_FEE = 10000;
+    uint public fee;
     uint public mintPrice;
     IERC20 public dom;
     address public dead = 0x000000000000000000000000000000000000dEaD;
+    address public feeOwner;
 
     event Mint(address indexed minter, address indexed holder, string _tokenURI,string _tag, string _cover, uint tokenId);
 
-    constructor(string memory _name, string memory _symbol, IERC20 _dom, uint _mintPrice) ERC721(_name,_symbol) {
+    constructor(string memory _name, string memory _symbol, IERC20 _dom, uint _mintPrice,address _feeOwner) ERC721(_name,_symbol) {
         dom = _dom;
         mintPrice = _mintPrice;
+        feeOwner = _feeOwner;
+    }
+
+    function setFee(uint _fee) public onlyOwner {
+        require(_fee<MAX_FEE,"error fee rate");
+        fee = _fee;
     }
 
     function _baseURI() internal view virtual override returns (string memory) {
@@ -32,7 +44,11 @@ contract Domino is ERC721URIStorage,ERC721Enumerable {
 
     function mint(address to, string memory _tokenURI, string memory _tag, string memory _cover) public virtual returns(uint tokenId)  {
         require(bytes(_tokenURI).length > 0, "uri should be set");
-        dom.safeTransferFrom(msg.sender, dead, mintPrice);
+
+        uint feeAmount = mintPrice.mul(fee)/MAX_FEE;
+        dom.safeTransferFrom(msg.sender, dead, mintPrice.sub(feeAmount));
+        if(feeAmount>0) dom.safeTransferFrom(msg.sender, feeOwner, feeAmount);
+        
         tokenId = _tokenIdTracker.current();
         _mint(to, tokenId);
         _setTokenURI(tokenId,_tokenURI);
--- deploy/Domino.js
+++ deploy/Domino.js
@@ -1,5 +1,5 @@
+require('dotenv').config()
 const ethers = require('ethers');
-const {BN} = require('bn.js');
 
 module.exports = async function ({ getNamedAccounts, deployments }) {
     const { deploy } = deployments
@@ -10,12 +10,13 @@ module.exports = async function ({ getNamedAccounts, deployments }) {
 
 
     const price = "1.0";
+    const feeOwner = process.env.FEE_OWNER;
 
 
     let priceBig = ethers.utils.parseUnits(price,18);
     await deploy("Domino", {
       from: deployer,
-      args: ["Dom nft coin","DNC",dtc, priceBig],
+      args: ["Dom nft coin","DNC",dtc, priceBig,feeOwner],
       log: true
     })
   }
